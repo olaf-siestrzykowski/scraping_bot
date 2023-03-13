196e04fedac8d1fab65dcda6f4cc4235a022c344
olfis <olfis@o2.pl>
2023-03-13 18:08:57 +0100

    2nd

--- reviews.py
+++ reviews.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 driver = webdriver.Chrome()
 driver.get("https://www.google.com/maps")
 
@@ -18,11 +17,11 @@ next_button2 = driver.find_element(By.CLASS_NAME, "whsOnd")
 next_button2.send_keys(email)
 next_button2.send_keys(Keys.RETURN)
 time.sleep(4)
+
 next_button3 = driver.find_element(By.CLASS_NAME, "whsOnd")
 next_button3.send_keys(password)
 next_button3.send_keys(Keys.RETURN)
 time.sleep(4)
-
 
 search_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
 search_box.send_keys("Pizza na wypasie")
@@ -34,16 +33,15 @@ next_button4[1].click()
 time.sleep(6)
 
 review_element = driver.find_element(By.CLASS_NAME, "m6QErb")
-review_elements = review_element.find_elements(By.XPATH, "*")
-reviews = [review.text for review in review_elements]
-print('s')
+review_elements = review_element.find_elements(By.CLASS_NAME, "jftiEf")
 
 for review in review_elements:
-    print(review)
+    review_el = review.find_element(By.CLASS_NAME, 'd4r55')
+    review_text = review_el.find_element(By.TAG_NAME, 'span')
+    print(review_text.text)
 
-for review in reviews:
-    print(review)
-
+print('s')
 search_box.submit()
 time.sleep(400)
 
+driver.quit()
