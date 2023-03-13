fb80b73b0f27a87c9342f0efec468050ffaa6705
olfis <olfis@o2.pl>
2023-03-13 16:54:05 +0100

    base code

---
+++ reviews.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/maps")
+
+next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
+next_button.click()
+email = "scraping17@gmail.com"
+password = "123qwe!@#QWE"
+
+next_button2 = driver.find_element(By.CLASS_NAME, "whsOnd")
+next_button2.send_keys(email)
+next_button2.send_keys(Keys.RETURN)
+time.sleep(4)
+next_button3 = driver.find_element(By.CLASS_NAME, "whsOnd")
+next_button3.send_keys(password)
+next_button3.send_keys(Keys.RETURN)
+time.sleep(4)
+
+
+search_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
+search_box.send_keys("Pizza na wypasie")
+search_box.send_keys(Keys.RETURN)
+time.sleep(7)
+
+next_button4 = driver.find_elements(By.CLASS_NAME, "hh2c6")
+next_button4[1].click()
+time.sleep(6)
+
+review_element = driver.find_element(By.CLASS_NAME, "m6QErb")
+review_elements = review_element.find_elements(By.XPATH, "*")
+reviews = [review.text for review in review_elements]
+print('s')
+
+for review in review_elements:
+    print(review)
+
+for review in reviews:
+    print(review)
+
+search_box.submit()
+time.sleep(400)
+
